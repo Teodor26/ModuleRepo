1d3312f55fc7ffcac2ede07f6d4a083ce42eae17
Teodor <teodorbear26@gmail.com>
2018-10-30 20:38:15 +0200

    My assingments is finished.

--- Module2/Module2/Class1.cs
+++ Module2/Module2/Class1.cs
@@ -14,15 +14,24 @@ namespace Module2
             double count = 30.0;
             for (int i = 0; i < ta.Length; i++)
             {
+
                 double time = ta[i].Time();
                 time = count-time;
                 if (time <= 0)
                 {
+                    Random rand = new Random();
+                    
+                    if(rand.Next(0,5)==1)
+                    {
+                        time = count - time;
+                    }
                     Console.WriteLine(ta[i].Solved);
                 }
             }
 
         }
+
+
 
 
     }
