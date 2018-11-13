b575b4ba33b39195c04ba99bb9d936402ca36205
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-11-13 21:36:19 +0200

    50%

---
+++ 1111/1111/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111
+{
+    class Class1
+    {
+        static void Main()
+        {
+            List<Person> lp = new List<Person>();
+            lp.AddRange(new[] {
+                new Person{ Name = "Kate", Age = 13},
+                new Person{ Name = "Kate", Age = 13},
+                new Person{ Name = "Kate", Age = 13},
+                new Person{ Name = "Kate", Age = 13},
+                new Person{ Name = "Kate", Age = 13},
+                new Person{ Name = "Kate", Age = 13}
+
+            });
+        }
+
+        
+    }
+
+    class Person
+    {
+        public string Name { get; set; } 
+        public int Age { get; set; }
+    }
+
+}
